372ae73080288ad01bfa947294ce2ce050727c86
Milky8 <82059730+Milky8@users.noreply.github.com>
2023-04-28 22:03:16 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 from linenotipy import Line
 #Your Account
-account_1 = ""
+account_1 = "_MHYUUID=44922aad-dbb1-4f7c-9aa3-2fa1c831d931; DEVICEFP_SEED_ID=b0d8fcfe984b2c66; DEVICEFP_SEED_TIME=1682539196123; G_ENABLED_IDPS=google; DEVICEFP=38d7ed122ba19; ltoken=u3zMuT1RN0eou5oCMkvNom7ryYla0HyLEpdZodtU; ltuid=43680158; _gid=GA1.2.269309302.1682720034; _ga_54PBK3QDF4=GS1.1.1682726315.3.1.1682728422.0.0.0; _ga_45QSFFBGJT=GS1.1.1682732366.3.0.1682732366.0.0.0; _ga_Y5SZ86WZQH=GS1.1.1682732369.6.0.1682732369.0.0.0; _ga=GA1.1.450026983.1632425946; _ga_SBYZMHZRMJ=GS1.1.1682732370.6.0.1682732370.0.0.0"
 
 #Line_Token
 Line_notify = False
 Line_TOKEN = Line(token='')
 
 #Discord_Webhook
-webhook = ''
+webhook = 'https://discord.com/api/webhooks/1101237890232631450/WV_MAzqXlcbjSvL66YJHM4lgv30CKBvVA6ftbVmW8yTgp3SHUQE7Oi75RtvYaHZSSTTI'
 
 #Discord_notify
-discord_notify = False
+discord_notify = True
 discord_server = [  
                     ''
                  ]
