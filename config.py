672d41f2361580da098b7f0a06b87d5d0071e398
Milky8 <82059730+Milky8@users.noreply.github.com>
2023-04-28 23:30:30 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from linenotipy import Line
 #Your Account
-account_1 = "_MHYUUID=44922aad-dbb1-4f7c-9aa3-2fa1c831d931; DEVICEFP_SEED_ID=b0d8fcfe984b2c66; DEVICEFP_SEED_TIME=1682539196123; G_ENABLED_IDPS=google; DEVICEFP=38d7ed122ba19; _gid=GA1.2.269309302.1682720034; _ga_54PBK3QDF4=GS1.1.1682726315.3.1.1682728422.0.0.0; _ga_45QSFFBGJT=GS1.1.1682735314.4.0.1682735314.0.0.0; _ga=GA1.2.450026983.1632425946; _ga_SBYZMHZRMJ=GS1.1.1682735072.7.1.1682736364.0.0.0; _ga_Y5SZ86WZQH=GS1.1.1682735072.7.1.1682736377.0.0.0; ltoken=u3zMuT1RN0eou5oCMkvNom7ryYla0HyLEpdZodtU; ltuid=43680158; cookie_token=wdWuAjUBdivuayeQap3IOtllnWwLADJE2jqLEgVs; account_id=43680158; login_ticket=a"
+
+account_1 = {'ltuid': ltuid, 'ltoken': ltoken}
 
 #Line_Token
 Line_notify = False
